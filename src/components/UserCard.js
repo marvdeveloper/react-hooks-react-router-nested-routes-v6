9399570c171e970f9b3439babca127c51c6952b3
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-01 21:45:44 +0300

    improve UserCard component layout and link accessibility

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 
-function UserCard({user}) {
-    return (
-      <article>
-          <h2>{user.name}</h2>
-          <Link to={`/profile/${user.id}`}>View profile</Link>
-      </article>
-    );
-  };
-  
-  export default UserCard;
+function UserCard({ user }) {
+  return (
+    <article className="p-4 rounded-xl shadow-md border border-gray-200 mb-4">
+      <h2 className="text-xl font-semibold mb-2">{user.name}</h2>
+      <Link
+        to={`/profile/${user.id}`}
+        className="text-blue-600 hover:underline"
+      >
+        View profile
+      </Link>
+    </article>
+  );
+}
+
+export default UserCard;
