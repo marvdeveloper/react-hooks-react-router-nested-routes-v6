6fbc7bb045f42067b407f1c338f2b3139c8ef0d4
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-01 21:46:31 +0300

    fetch and display user list on Home page with NavBar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,19 @@
-import { useState, useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
 import UserCard from "../components/UserCard";
-import NavBar from "../components/NavBar";
 
 function Home() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() =>{
-    fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>;
-  });
+  const users = useOutletContext(); // Get users from context passed by App.js
 
   return (
-    <>
-      <header>
-        <NavBar />
-      </header>
-      <main>
-        <h1>Home!</h1>
-        {userList}
-      </main>
-    </>
+    <main>
+      <h1>Home!</h1>
+      {users.length > 0 ? (
+        users.map(user => <UserCard key={user.id} user={user} />)
+      ) : (
+        <p>Loading users...</p>
+      )}
+    </main>
   );
-};
+}
 
 export default Home;
