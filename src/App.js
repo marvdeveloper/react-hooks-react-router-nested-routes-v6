3b52039cb2a05d49ccf397b2eede37436dc859ec
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-01 21:47:13 +0300

    set up App component structure with header placeholder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar"; // Optional if you have one
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+      .then((r) => r.json())
+      .then(setUsers)
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
       <header>
-        
+        <NavBar />
       </header>
+      <main>
+        <Outlet context={users} />
+      </main>
     </>
   );
-};
+}
 
 export default App;
